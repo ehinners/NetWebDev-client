ed7ab2022017bba1589e881a1418db4092ea3b04
ehinners <ehinners@my.wctc.edu>
2022-02-07 12:47:27 -0600

    Check/uncheck all balloons with a single click

--- js/balloons.js
+++ js/balloons.js
@@ -1,9 +1,10 @@
 $(function(){
 
+    // create array of "attention seeker" names (still needs class name prefix)
     let animationList = ["heartBeat", "bounce", "flash", "pulse", "rubberBand", "shakeX", "shakeY", "headShake", "swing", "tada", "wobble", "jello"]
-    
+    // picks one at random
     var atnSeeker = animationList[Math.floor(Math.random() * animationList.length)];
-
+    // adds prefix to complete class name, then applies it to appropriate element
     var prefix =    "animate__";
     var animation = prefix + atnSeeker;
     $("#randomAnimateTitle").addClass(animation);
@@ -16,6 +17,20 @@ $(function(){
         $(this).prop('checked', false);
     });
 
+    var allButtonsChecked = false;
+
+    // event listener for select all button
+    $('#checkAllBalloons').on('click', function() {
+        allButtonsChecked = !allButtonsChecked;
+
+        // check all checkboxes
+        $('.form-check-input').each(function () {
+             //$(this).prop('checked', allButtonsChecked);
+             $(this)[0].checked = allButtonsChecked;
+             $(this).trigger("change");
+        });
+    });
+
     // event listener for check/uncheck
     $('.form-check-input').on('change', function () {
         // make the image visible
@@ -25,4 +40,12 @@ $(function(){
          $('#' + this.id + 'Img').removeClass().addClass('animate__animated animate__bounceInDown') :
          $('#' + this.id + 'Img').addClass('animate__animated animate__bounceOutUp');
     });
+
+    // event listener for form submit
+    $('#submit').on('click', function(event) {
+        event.preventDefault();
+        
+    });
+
+    
 });
